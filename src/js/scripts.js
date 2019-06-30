37524922ad1730127956448b4def2940ebf7df08
Jason Graziano <mrehawk82@gmail.com>
2019-06-30 16:54:40 -0400

    ...

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,23 @@ var myUI;
 
 myUI = {
 	init: function(){
-		console.log("alert: game initializing now");
+		var frame = createEle("div"),
+		    header = createEle("header"),
+		    container = createEle("div"),
+		    footer = createEle("footer");
+
+		footer.innerHTML = "2020 EHAWK LLC";
+
+		container.innerHTML = "divs n stuff";
+		container.className = "container";
+
+		header.innerHTML = "<h1>SIM GAME</h1>";
+
+		frame.className = "frame";
+		frame.append(header,container,footer);
+
+		body.append(frame);
+		//console.log("alert: game initializing now");
 	}
 };
 
